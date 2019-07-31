2fb0d4e8407fead3906afdc0007dfa9ea76d19ee
Ricardo Dias <cadu.hd@gmail.com>
2019-07-31 06:59:44 -0300

    adicionar navegação, botões de face e google

--- lib/login/login.page.dart
+++ lib/login/login.page.dart
@@ -1,99 +1,198 @@
+import 'package:balta_flutter_animations/login/signup.page.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Color(0xFFF5F5F5), // cor com hexadecimal
-        padding: EdgeInsets.only(top: 80, left: 20, right: 20, bottom: 40),
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 450,
-              decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                new BoxShadow(
-                  color: Colors.black12,
-                  offset: new Offset(1, 2.0), // distancia da borda
-                  blurRadius: 5, // expansao da borda
-                  spreadRadius: 1,
-                ),
-              ]),
-              child: Padding(
-                padding: EdgeInsets.all(5),
-                child: ListView(
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Column(
-                          children: <Widget>[
-                            Text(
-                              "Welcome",
-                              style: TextStyle(
-                                fontSize: 28,
-                                fontWeight: FontWeight.w500,
+      body: SingleChildScrollView(
+        child: Container(
+          color: Color(0xFFF5F5F5), // cor com hexadecimal
+          padding: EdgeInsets.only(top: 80, left: 20, right: 20, bottom: 40),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 400,
+                decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                  new BoxShadow(
+                    color: Colors.black12,
+                    offset: new Offset(1, 2.0), // distancia da borda
+                    blurRadius: 5, // expansao da borda
+                    spreadRadius: 1,
+                  ),
+                ]),
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    left: 15,
+                    right: 15,
+                    top: 30,
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Text(
+                                "Welcome",
+                                style: TextStyle(
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.w500,
+                                ),
                               ),
-                            ),
-                            Text("Sign in to continue"),
-                          ],
+                              Text("Sign in to continue"),
+                            ],
+                          ),
+                          FlatButton(
+                            child: Text("Sign Up"),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SignUpPage()),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          labelStyle: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                          ),
                         ),
-                        FlatButton(
-                          child: Text("Sign Up"),
+                        style: TextStyle(
+                          // estilo do texto digitado no input
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          labelStyle: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                          ),
+                        ),
+                        style: TextStyle(
+                          // estilo do texto digitado no input
+                          fontSize: 20,
+                        ),
+                      ),
+                      Container(
+                        height: 40,
+                        alignment: Alignment.centerRight,
+                        child: FlatButton(
+                          child: Text("Forgot your password?"),
                           onPressed: () {},
                         ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 60,
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        labelText: "Email",
-                        labelStyle: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 16,
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
+                        ),
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Sign In",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
                       ),
-                      style: TextStyle(
-                        // estilo do texto digitado no input
-                        fontSize: 20,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: "Password",
-                        labelStyle: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 16,
-                        ),
-                      ),
-                      style: TextStyle(
-                        // estilo do texto digitado no input
-                        fontSize: 20,
-                      ),
-                    ),
-                    Container(
-                      height: 40,
-                      alignment: Alignment.centerRight,
-                      child: FlatButton(
-                        child: Text("Forgot your password?"),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+              Container(
+                padding: EdgeInsets.only(top: 15, bottom: 15),
+                child: Text(
+                  "- OR -",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 40,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2.0,
+                    color: Colors.blue,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5),
+                  ),
+                ),
+                child: FlatButton(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        height: 24,
+                        width: 100,
+                        child: Image.asset("assets/facebook.png"),
+                      ),
+                      Text("Sign In with Facebook"),
+                    ],
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: double.infinity,
+                height: 40,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2.0,
+                    color: Colors.blue,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5),
+                  ),
+                ),
+                child: FlatButton(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        height: 24,
+                        width: 100,
+                        child: Image.asset("assets/google.png"),
+                      ),
+                      Text("Sign In with Google"),
+                    ],
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
