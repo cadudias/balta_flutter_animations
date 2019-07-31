f3101ee8da9c7a03b4f2551297ca4077193450bb
Ricardo Hofstetter <ricardo.hofstetter@redasset.com.br>
2019-07-31 17:38:13 -0300

    adicionado tela de signup

--- lib/login/signup.page.dart
+++ lib/login/signup.page.dart
@@ -4,8 +4,141 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: (Text("Sign Up page")),
+      
+      appBar: AppBar(
+        backgroundColor: Colors.white.withOpacity(0),
+        elevation: 0,
+        //title: Text("Teste"),
+        //leading: Text("1"),
+        // actions: <Widget>[
+        //   Icon(Icons.favorite),
+        // ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          color: Color(0xFFF5F5F5),
+          padding: EdgeInsets.only(
+            top: 80,
+            left: 20,
+            right: 20,
+            bottom: 40
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 450,
+                decoration: BoxDecoration(
+                  color: Colors.white, 
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.black12,
+                      offset: new Offset(1, 2.0), // distancia da borda
+                      blurRadius: 5, // expansao da borda
+                      spreadRadius: 1,
+                    ),
+                  ],
+                ),
+                child: Padding(
+                    padding: EdgeInsets.only(
+                    left: 15,
+                    right: 15,
+                    top: 30,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                       Text(
+                        "Signup",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: "Nome",
+                          labelStyle: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                          ),
+                        ),
+                        style: TextStyle(
+                          // estilo do texto digitado no input
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          labelStyle: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                          ),
+                        ),
+                        style: TextStyle(
+                          // estilo do texto digitado no input
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          labelStyle: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                          ),
+                        ),
+                        style: TextStyle(
+                          // estilo do texto digitado no input
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
+                        ),
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Sign In",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
