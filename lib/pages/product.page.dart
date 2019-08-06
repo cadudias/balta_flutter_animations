f822a1b6801f0100d56a66b81eb77e5dedfce49d
Ricardo Hofstetter <ricardo.hofstetter@redasset.com.br>
2019-08-06 11:24:41 -0300

    adicionar nested scroll view com slider scroll view

--- lib/pages/product.page.dart
+++ lib/pages/product.page.dart
@@ -4,11 +4,29 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.blue,
-        child: Center(
-          child: Text("Detalhes do produto"),
-        ),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsCrolled) 
+        {
+          return <Widget> [
+            SliverAppBar(
+              backgroundColor: Colors.white.withOpacity(0),
+              elevation: 0.0,
+              expandedHeight: 450.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                background: Image.asset(
+                  "assets/product-10.png", 
+                  width: double.infinity,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+          ];
+        },
+        body: Container(),
+        
       ),
     );
   }
